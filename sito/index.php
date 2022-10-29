93853d6f4b93812e5baab2b0c3efa04ba8db56bd
Eyrtxnuo <Mattiabossi2004@gmail.com>
2022-10-29 11:41:42 +0200

    Fix and comment index.php
    Removed useless try-catch,
    Fixed soft-404, now the response code is 404,
    Commented file

--- sito/index.php
+++ sito/index.php
@@ -1,29 +1,31 @@
 <?php
 
+    # .htaccess transform the requested url to this file, 
+    #  with the old local path as the argument.
     $request = $_SERVER['REQUEST_URI'];
-    
+
+    # split the old path from the URL parameters.
     $param = explode('?', $request);
-    try{
-        switch (rtrim($param[0], '/')) {
-            case '/':
-            case '':
-            case '/home':
-                require __DIR__ . '/views/homepage.php';
-                break;
-            case '/login':
-                require __DIR__ . '/views/login.php';
-                break;
-            case '/register':
-                require __DIR__ . '/views/register.php';
-                break;
-            case '/dashboard':
-                require __DIR__ . '/views/dashboard.php';
-                break;
-            default:
-                require __DIR__ . '/errors/404.php';
-                break;
-        }
-    }catch(Exception $e){
-        require __DIR__ . '/errors/404.php';
+    
+    #remove trailing slashes and load the correct webpage.
+    switch (rtrim($param[0], '/')) {
+        case '/':
+        case '':
+        case '/home':
+            require __DIR__ . '/views/homepage.php';
+            break;
+        case '/login':
+            require __DIR__ . '/views/login.php';
+            break;
+        case '/register':
+            require __DIR__ . '/views/register.php';
+            break;
+        case '/dashboard':
+            require __DIR__ . '/views/dashboard.php';
+            break;
+        default:
+            http_response_code(404); # set HTTP error code to 404: Not Found, if the route is not found.
+            require __DIR__ . '/errors/404.php';
+            break;
     }
 ?>
